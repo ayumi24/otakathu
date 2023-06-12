d51cf4416b4b7d0ffdd06edb61d9dec0f2eb1002
ayumi24 <115326506+ayumi24@users.noreply.github.com>
2023-06-12 07:09:02 +0000

    [Add]controler,view作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,15 @@
 Rails.application.routes.draw do
 
-  devise_for :admins
-  devise_for :customers
-  root to: "homes#top"
+  #ユーザー用ルーティング
+  devise_for :customers, skip: [:passwords], controllers: {
+  registrations: "public/registrations",
+  sessions: 'public/sessions'
+}
+
+ #管理者用ルーティング
+ devise_for :admins, skip: [:registrations, :passwords], controllers: {
+   sessions: 'admin/sessions'
+ }
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
