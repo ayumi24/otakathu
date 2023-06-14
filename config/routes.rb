71121ccb6a2d220b9042a4c806f44dbc1c3c64b9
ayumi24 <115326506+ayumi24@users.noreply.github.com>
2023-06-14 12:23:37 +0000

    [Add]Topページ、Aboutページ作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+ root to: "public/homes#top"
+
   #ユーザー用ルーティング
   devise_for :customers, skip: [:passwords], controllers: {
   registrations: "public/registrations",
@@ -10,6 +12,10 @@ Rails.application.routes.draw do
  devise_for :admins, skip: [:registrations, :passwords], controllers: {
    sessions: 'admin/sessions'
  }
+
+ namespace :public do
+  get 'homes/about'
+ end
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
